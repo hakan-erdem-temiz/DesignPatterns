3a23be10ee84ad94e62dd5dba74143fe47e0e9b9
hakanerdemtemiz <hakanerdemtemiz@gmail.com>
2018-09-30 13:36:51 +0300

    FactoryPattern

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory sf = new ShapeFactory();
+            Shape shape= sf.getShapes("square");
+            shape.draw();
+            Console.ReadKey();
+        }
+    }
+} 
---
+++ FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,36 @@
+﻿using FactoryPattern.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FactoryPattern
+{
+    class ShapeFactory
+    {
+        public Shape getShapes(string ShapeName)
+        {
+            if (ShapeName == null)
+            {
+                return null;
+            }else if (string.Equals(ShapeName, "Circle", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Circle();
+            }
+            else if (string.Equals(ShapeName, "Rectangle", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Rectangle();
+            }
+            else if (string.Equals(ShapeName, "Square", StringComparison.OrdinalIgnoreCase))
+            {
+                 return new Square();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
